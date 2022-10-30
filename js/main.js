9e5a9200cf5ff1d26ba229def8ff050aee462aa0
YoctoBit <42770197+YoctoBit@users.noreply.github.com>
2022-10-30 21:40:25 +0400

    Функция для генерации данных в main.js добавлена

--- js/main.js
+++ js/main.js
@@ -5,6 +5,43 @@ const getRandomInt = function (min, max) {
 
 // Функция для проверки максимальной длины строки.
 const commentIsToLong = function (maxlengthСomments, comment) {
-  return comment.length <= maxlengthСomments ? true : false;
+  return comment.length <= maxlengthСomments;
 };
 
+const photoDescription = {
+adjective: ['Интересная',
+  'Прикольная',
+  'Занимательная',
+  'Красивая',
+  'Любопытнвя'],
+
+  noun: ['картинка',
+  'картиночка',
+  'фоточка',
+  'фотография',
+  'фотка'],
+  
+  addition: ['для истинных ценителей',
+  'от профи',
+  'немного мрачноватая',
+  'без обработки',
+  'сделана с любовью']
+}
+
+const getArrOfObject = function(amount){
+  Arr = [];
+  for (let i=0; i<= amount; i++){
+    Arr[i] = {
+      id: i+1,
+      url: 'photos/' + i+1 +'.jpg',
+      description: photoDescription.adjective[getRandomInt(1, 5)] + ' ' + 
+        photoDescription.noun[getRandomInt(1, 5)] + ' ' +
+        photoDescription.addition[getRandomInt(1, 5)],
+      likes: getRandomInt(15, 200),
+      comments: getRandomInt(0, 200)
+    }
+  }
+  return Arr;
+}
+
+getArrOfObject (25);
